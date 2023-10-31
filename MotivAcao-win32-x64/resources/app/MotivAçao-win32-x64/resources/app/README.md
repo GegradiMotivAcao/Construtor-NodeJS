487740cbe8c425f3f0e23beb36f0a99285456a62
PedroThiagoRoque <pedronmroque@gmail.com>
2023-10-31 12:00:26 -0300

    v1.1.5 - revisada

---
+++ MotivAcao-win32-x64/resources/app/MotivAçao-win32-x64/resources/app/README.md
@@ -0,0 +1,47 @@
+# Construtor / Jogos MotivAção - GEGRADI - UFPel
+
+Repositório para projeto de Builder Local para jogos na plataforma GEGRADI - MotivAção. <br>
+Esta plataforma tem como proposito facilitar a criação de jogos digitais que utilizem o método MotivaAção: A edição de imagens urbanas  que motivem nos 
+usuários o desejo de transformação dos espaços.<br> 
+Com base nos resultados dos feedbacks de ações extensionistas, a criação desta plataforma foi guiada pela proposta da geração de jogos sem a 
+necessidade de adquirir conhecimento em programação e desenvolvimento de software, expandindo o público alvo (extensionistas, professores, alunos).
+
+<br>
+
+<h1>Requisitos: </h1>
+<ul>
+<li> Baixe o repositório de projeto unity em seu computador (.zip, ou via github desktop) e descompacte. Será este projeto de jogo que utilizaremos no construtor.<br> 
+   <B> LINK: </B> https://github.com/GegradiMotivAcao/Motivacao-Builder-LOCAL
+</li>
+<li> Baixe e instale o Unity 2018.4.35 LTS. <br>
+WIN: https://download.unity3d.com/download_unity/dbb5675dce2d/UnityDownloadAssistant-2018.4.35f1.exe <br>
+<B>IMPORTANTE:</B> Não é necessário abrir o Unity para o construtor operar. Apenas ter o unity 2018.4 em sua máquina.
+</li>
+<li> Baixe a última versão do Construtor - MotivaAção em seu computador (.zip, ou via github desktop) e descompacte. Nesta pasta está o executável do Construtor.<br> 
+   <B> LINK: </B> https://github.com/GegradiMotivAcao/Construtor-NodeJS/releases
+</li>
+</ul>
+<br>
+        <h1>Passo a passo:</h1>
+            <br>
+            <ul>
+                <li><b>Local do Unity</b> - Devemos selecionar o local da instalação de nosso unity (unity.exe). Clique em 
+                "Selecione Local" para abrir a janela de seleção e aponte o local do unity em seu computador.</li>
+                <li><b>Local do Projeto</b> - Devemos selecionar o local do projeto MotivaAção que você utilizará para criar seu jogo. Clique em "Selecione Local" para 
+                abrir a janela de seleção e aponte o local da pasta do projeto em seu computador.</li>
+                <li><b>Plano de Fundo</b> - Devemos selecionar uma imagem de fundo para a cena do jogo, dando preferência aos formatos <b>.jpg</b> e <b>.png</b>. Clique em "Adicionar fundo" para 
+                abrir a janela de seleção de imagem.</li>
+                <li><b>Componentes</b> -Devemos selecionar imagens de componentes para a cena do jogo. Selecione quantas quiser. <b>Importante:</b> As imagens de componentes devem ser em formato <b>.png </b>
+                    sem fundo e com dimensões <b>1366 x 768</b> para melhor encaixe na cena. Clique em "Adicionar imagens" para abrir a janela de seleção.</li>
+                <br>
+                <li><b>Contruir!</b> - Após selecionar as 4 partes para seu jogo, clique em "Construir!" para começar a construção. Ao terminar o contrutor abrirá a pasta onde se encontra seu projeto, na pasta <b>EXECUTAVEL.</b> </li>
+              </ul>
+        </p>
+        <br>
+        <h1>Créditos</h1>
+        <p>Adriane Borda Almeida da Silva - Orientadora <br>
+        Pedro Thiago do Nascimento Moreira Roque - Desenvolvedor<br>
+        Gabriel da Silva Bittencourt - Desenvolvedor<br>
+        Bruno Iost - Desenvolvedor
+        </p>
+        <br>
---
+++ MotivAcao-win32-x64/resources/app/main.js
@@ -0,0 +1,217 @@
+const {app, BrowserWindow, ipcMain, dialog, shell } = require('electron')
+const { exec } = require("child_process"); //permite executar linha no cmd/terminal
+const path = require('path')
+const fs = require('fs');
+
+let enderecos = [];
+let enderecoSelect= {};
+let Unity="";
+let Projeto="";
+
+let OKunity = false;
+let OKproj = false;
+let OKbg = false;
+let OKimg = false;
+//RODAR: npm run start
+
+//FUNÇÃO QUE ABRE O DIALOG DE SELECIONAR ARQUIVOS
+async function handleFileOpen() {
+  const { canceled, filePaths } = await dialog.showOpenDialog()
+  if (canceled) {
+    return
+  } else {
+  console.log("func dialog: " + filePaths[0])
+    return filePaths[0]
+  }
+}
+
+//FUNÇÃO QUE ABRE O DIALOG DE SELECIONAR PASTAS
+async function handleFolderOpen() {
+  const { canceled, filePaths } = await dialog.showOpenDialog({ properties: ['openDirectory'] })
+  if (canceled) {
+    return
+  } else {
+  console.log("func dialog: " + filePaths[0])
+    return filePaths[0]
+  }
+}
+
+//escreve caminho do Background no arquivo
+ipcMain.on('escreveBackground', async () => {
+  const pathdoarq = await handleFileOpen();
+  nomeArq = path.basename(pathdoarq);
+  //copia a imagem para o \resources do projeto unity
+  exec( 'copy '+ '"' + pathdoarq +'" '+ '"' + Projeto + '\\Assets\\Resources\\backgrounds"', (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: SUCESSO! ${stdout}`);
+    fs.appendFile(Projeto + '\\Assets\\Resources\\lista.txt', "backgrounds\\" + nomeArq + ";0;0\n", function (err) {
+      if (err) throw err;
+      console.log('BG Salvo!');
+    });
+    OKbg = true;
+});
+
+});
+
+//escreve caminhos de Elementos no arquivo
+ipcMain.on('escreveElemento', async () => {
+  const pathdoarq = await handleFileOpen();
+  nomeArq = path.basename(pathdoarq);
+
+  exec( 'copy '+ '"' + pathdoarq +'" '+ '"' + Projeto + '\\Assets\\Resources\\objetos"', (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    fs.appendFile(Projeto + '\\Assets\\Resources\\lista.txt', "objetos\\"+ nomeArq + ";1;0\n", function (err) {
+      if (err) throw err;
+      console.log('Elemento Salvo!');
+    });
+    OKimg = true;
+});
+
+})
+
+//Recebe o endereço do unity
+ipcMain.on('LocalizaUnity', async () => {
+  const pathdoUnity = await handleFileOpen();
+  Unity = pathdoUnity;
+  console.log("Unity: "+ pathdoUnity);
+  if (pathdoUnity) {
+    OKunity = true;
+  }
+})
+
+//Recebe o endereço do projeto
+ipcMain.on('LocalizaProjeto', async () => {
+  const pathdoProjeto = await handleFolderOpen();
+  Projeto = pathdoProjeto;
+  console.log("Proj: "+ pathdoProjeto);
+  
+  if(pathdoProjeto){
+    OKproj=true;
+  }
+})
+
+ipcMain.on('AbreAjuda', async () => {
+
+  const mainWindow = new BrowserWindow({
+    autoHideMenuBar: true,
+    width: 850, height: 700,
+    resizable: false
+
+  })
+  mainWindow.loadFile('views/ajuda.html')
+
+})
+
+//Função que chama a linha de comando no prompt/terminal
+ipcMain.on('Executacomando', function ExecutaComando() {
+
+  const options = {
+    type: 'question',
+    buttons: ['OK'],
+    defaultId: 2,
+    title: 'Erro!',
+    message: 'Você precisa selecionar uma instalação do unity. (Ex: "unity.exe")',
+  };
+
+  if(!OKunity) {
+    console.log("Falta unity!") 
+    dialog.showMessageBox(null, options);
+    return
+  }
+  if(!OKproj) {
+    console.log("Falta projeto!") 
+    options.message = 'Você precisa selecionar uma pasta de projeto MotivaAção. '
+    dialog.showMessageBox(null, options);
+    return
+  }
+  if(!OKbg) {
+    console.log("Falta BG!") 
+    options.message = 'Você precisa selecionar uma imagem de fundo para sua cena.'
+    dialog.showMessageBox(null, options);
+    return
+  }
+  if(!OKimg) {
+    console.log("Falta imagens!") 
+    options.message = 'Você precisa selecionar uma ou mais imagens de componentes para sua cena.'
+    dialog.showMessageBox(null, options);
+    return
+  }
+
+ const mainWindow = new BrowserWindow({
+    autoHideMenuBar: true,
+    width: 1200, height: 600,
+  })
+  mainWindow.loadFile('views/loading.html')
+
+  exec( '"'+ Unity +'" -batchmode -quit -projectPath '+ Projeto +' -executeMethod BuilderLinhaComando.PerformBuild', (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: SUCCESS ${stdout}`);
+    mainWindow.close();
+
+    shell.openPath(Projeto);
+    //limpa as referências
+    fs.writeFileSync(Projeto + '\\Assets\\Resources\\lista.txt', "", function (err) {
+      if (err) throw err;
+      console.log('Referências limpas!');
+    });
+});
+})
+
+
+/////////////////////////////////////////
+//FUNÇÕES DE JANELA DO ELECTRON
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }, resizable: false,
+    icon: path.join(__dirname, 'skins/logoMotiv.png') 
+  })
+  mainWindow.loadFile('views/construtor.html')
+}
+
+ipcMain.on('resize-window', (event, width, height) => {
+    let browserWindow = BrowserWindow.fromWebContents(event.sender)
+    browserWindow.setSize(width,height)
+})
+
+
+app.whenReady().then(() => {
+  ipcMain.handle('dialog:openFile', handleFileOpen)
+  createWindow()
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+///////////////////////////////////////////////////////////
+//build com electron: 
+//npx electron-packager <sourcedir> <appname> --platform=win32
